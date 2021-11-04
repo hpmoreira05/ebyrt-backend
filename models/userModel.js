29687ee555931c99fd71a6bdb50b7b234b365145
Henrique Pozzolini <hpmoreira05@gmail.com>
2021-11-04 15:31:07 -0300

    Add cors

--- models/userModel.js
+++ models/userModel.js
@@ -2,7 +2,7 @@ const db = require('./connection');
 
 const createUser = async ({ email, password, name }) => {
   const inserted = await db.collection('users').insertOne({ name, email, password });
-  return { user: { name, email, role: 'user', _id: inserted.insertedId } };
+  return { user: { name, email, _id: inserted.insertedId } };
 };
 
 const getEmail = async (email) => {
